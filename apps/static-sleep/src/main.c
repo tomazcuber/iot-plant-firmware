ef67156f2c0b1886e422304830151afc344bc333
tomazcuber <tomazcuber@outlook.com>
2026-04-26 20:58:22 -0300

    feat: implement static-sleep baseline firmware

--- apps/static-sleep/src/main.c
+++ apps/static-sleep/src/main.c
@@ -1,1 +1,11 @@
-int main(void) { return 0; }
+#include <zephyr/kernel.h>
+#include <zephyr/pm/pm.h>
+
+int main(void)
+{
+    pm_state_force(0u, &(struct pm_state_info){
+        PM_STATE_SOFT_OFF, 0, 0
+    });
+    k_sleep(K_FOREVER);
+    return 0;
+}
